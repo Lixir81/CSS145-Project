a59cbfeb0a317a600481c3d75575ff62e4c0726d
Tyrell <153912747+Lixir81@users.noreply.github.com>
2024-11-10 20:01:54 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 import plotly.express as px
+import io
 
 from PIL import Image
 
